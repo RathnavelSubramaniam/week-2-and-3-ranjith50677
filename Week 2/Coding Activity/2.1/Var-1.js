38e8f10b4868a35607cc4d3c9dd3de1bbc11a2a2
ranjith50677 <91112010+ranjith50677@users.noreply.github.com>
2021-10-20 11:08:12 +0530

    Add files via upload

---
+++ Week 2/Coding Activity/2.1/Var-1.js
@@ -0,0 +1,17 @@
+/*Instructions
+ - Greet your friend by printing a message to the console.
+*/
+var myFriend="undertaker"
+///////////////
+function greetings() {
+    greetings='undertaker'
+      return myFriend;
+      ///////////////
+    
+    }
+    //leave this line unchanged to console log the results
+    console.log('results: ', greetings());
+    //dont change this line
+    if (typeof module !== 'undefined') {
+      module.exports = greetings;
+    }
